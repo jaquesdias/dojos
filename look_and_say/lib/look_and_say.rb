b4288f48c92e322603b9935ef5b2ff51c163781e
Jaques Dias <jaquesdias@Jaquess-MacBook-Air.local>
2015-10-20 11:20:35 -0200

    Identacao de arquivos e refatoracao do Look and Say

--- look_and_say/lib/look_and_say.rb
+++ look_and_say/lib/look_and_say.rb
@@ -1,45 +1,33 @@
 #http://dojopuzzles.com/problemas/exibe/sequencia-look-and-say/
 class LookAndSay
-	attr_reader :number
+  attr_reader :number
 
-	def initialize(number)
-		@number = number
-	end
+  def initialize(number)
+    @number = number
+  end
 
-	def validate
-		@number.is_a?(Integer) && @number >=1 && @number <=9
-	end
+  def validate
+    @number.is_a?(Integer) && @number >=1 && @number <=9
+  end
 
-	def build_next_element(sequence)
-		count = 1
-		new_sequence = ""
-		last_item_of_sequence = sequence.last.split("")
-		last_item_of_sequence.each_with_index do |v,i|
-			if v == last_item_of_sequence[i+1]
-				count += 1
-			else
-				str = "#{count}#{v}"
-				new_sequence << str
-				count = 1
-			end
-		end
-		new_sequence
-	end
+  def build_next_element(sequence)
+    sequence.last.gsub!(/(.)\1*/){|s| s = s.size.to_s + s[0]}
+  end
 
-	def get_sequence(element)
-		if validate
-			sequence = [@number.to_s]
-			(element).times do |k|
-				sequence << build_next_element(sequence)
-			end
-			sequence[element]
-		end
-	end
+  def get_sequence(element)
+    if validate
+      sequence = [@number.to_s]
+      (element).times do |k|
+        sequence << build_next_element(sequence)
+      end
+      sequence[element]
+    end
+  end
 
-	def get_sum_of_element_digits(element)
-		if validate
-			result = get_sequence(element)
-			result.split("").map{|char| char = char.to_i}.inject(:+)
-		end
-	end
+  def get_sum_of_element_digits(element)
+    if validate
+      result = get_sequence(element)
+      result.split("").map{|char| char = char.to_i}.inject(:+)
+    end
+  end
 end
